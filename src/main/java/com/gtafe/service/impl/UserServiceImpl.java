2d5a4ef5b6a9de927b8a3b1005ee61e283a5dad1
free <li5220008@163.com>
2014-04-17 19:37:30 +0800

    springMVC

---
+++ src/main/java/com/gtafe/service/impl/UserServiceImpl.java
@@ -0,0 +1,74 @@
+package com.gtafe.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gtafe.dao.IUserOperation;
+import com.gtafe.model.User;
+import com.gtafe.service.IUserService;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+	@Autowired
+    IUserOperation userMapper;
+
+	public void addUser(User user) {
+		userMapper.addUser(user);
+	}
+
+	public void deleteUser(int id) {
+		userMapper.deleteUser(id);
+	}
+
+	public List<User> findAllUser() {
+		List<User> userList=userMapper.findAllUser();
+		return userList;
+	}
+
+	public User login(Map map) {	  
+		return  userMapper.login(map);
+	}
+
+	public User selectUserByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> selectUsers(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+    public List<User> getDataByPage(Map map){
+    	return userMapper.getDataByPage(map);
+    };
+    public int getDataCount(String sql){
+    	return userMapper.getDataCount(sql);
+    };
+    
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public int addUserList(List<User> userList) throws Exception{  
+    	try {
+			for (int i = 0; i <userList.size(); i++) {
+				User user = userList.get(i);
+				if(i==8){
+					user.setPassword(null);
+				}
+				userMapper.addUser(user);
+			}
+		} catch (Exception e) {			
+				 throw e;//注意，这里必须是重新抛出，让spring来处理，不然是不会rollback的。
+		}
+		return 10;
+    }
+}
---
+++ src/main/java/com/gtafe/web/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.gtafe.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Desc:
+ * User: weiguili(li5220008@gmail.com)
+ * Date: 14-4-17
+ * Time: 上午11:28
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+}
