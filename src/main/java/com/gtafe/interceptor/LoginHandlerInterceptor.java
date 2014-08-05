8f3128b3c486eb64d0817306a97131b13a81b4d5
free <li5220008@163.com>
2014-08-05 19:15:49 +0800

    更新

---
+++ src/main/java/com/gtafe/interceptor/LoginHandlerInterceptor.java
@@ -0,0 +1,27 @@
+package com.gtafe.interceptor;
+
+import com.gtafe.model.User;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginHandlerInterceptor extends HandlerInterceptorAdapter {
+
+	/**
+	 * 每次调用方法前进行预判，只有存在session才能执行对应方法
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+			HttpSession session = request.getSession();
+			User user= (User) session.getAttribute("account");
+			if(user!=null){
+				return true;
+			}else{
+				response.sendRedirect(request.getContextPath()+"/user/login");
+				return false;
+			}
+		}
+}
